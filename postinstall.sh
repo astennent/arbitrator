17ae2b21a74f20037cab3cb1520fe7d6a538517c
Alan Tennent <atennent@factset.com>
2016-10-24 23:16:15 -0700

    Why no gulp

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x #echo on
 
-node node_modules/bower/bin/bower install
-sleep 1
-node node_modules/gulp/bin/gulp build
+node ./node_modules/bower/bin/bower install
+npm install gulp
+node ./node_modules/gulp/bin/gulp build
