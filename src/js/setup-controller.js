27f3f708764012fbba7069fc54e7c1f034b81e83
Alan Tennent <atennent@factset.com>
2016-11-07 21:32:20 -0800

    Update default coder id header

--- src/js/setup-controller.js
+++ src/js/setup-controller.js
@@ -1,7 +1,7 @@
 app.controller('setupController', ['$scope', 'coderData', function($scope, coderData) {
 
    $scope.caseIdKey = 'Q2 - ID #'; // TODO: Don't hard-code these.
-   $scope.coderIdKey = 'Q100 - Name of Coder';
+   $scope.coderIdKey = 'Q101 - Name of Coder';
 
    $scope.handleLoad = function(fileContents) {
       coderData.importRawData(fileContents, $scope.caseIdKey, $scope.coderIdKey);
